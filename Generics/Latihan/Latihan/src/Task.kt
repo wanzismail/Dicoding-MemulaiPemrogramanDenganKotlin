23374f2a77945121797108d96cff94dca4435e07
mailQasir <ridwanismail@qasir.id>
2020-08-08 19:44:56 +0700

    Done

---
+++ Generics/Latihan/Latihan/src/Task.kt
@@ -0,0 +1,23 @@
+/**
+ * Untuk menyelesaikan tugas latihan, Anda tidak diperbolehkan mengubah struktur kode yang sudah ada. Kecuali:
+ *    - Untuk melakukan improvisasi kode
+ *    - Mengikuti perintah yang ada
+ *
+ * Cukup tambahkan kode berdasarkan perintah yang sudah ditentukan.
+ *
+ */
+
+fun main() {
+    val stringResult = getResult("Kotlin")
+    val intResult = getResult(100)
+
+    // TODO 2
+    println("$stringResult\n$intResult")
+}
+
+// TODO 1
+fun <T> getResult(args: T) = when (args) {
+    is Int -> args * 5
+    is String -> args.length
+    else -> 0
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Memulai Pemrograman Dengan Kotlin
+Baparekraf Developer Day 2020
+- Learning Materials from [Dicoding (Memulai Pemrograman Dengan Kotlin)](https://www.dicoding.com/academies/80)
